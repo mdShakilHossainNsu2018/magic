13ed551b001e72189e595470d626ddefd64a9d2f
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2021-06-08 22:27:09 +0600

    bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Ask Me Anything"),
+            backgroundColor: Colors.blue,
+          ),
+          body: HomePage(),
+        ),
       ),
     );
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int number = 1;
+
+  void updateState() {
+    setState(() {
+      number = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.blue[300],
+        child: GestureDetector(
+            onTap: () {
+              updateState();
+            },
+            child: Image.asset("assets/images/ball$number.png")),
+      ),
+    );
+  }
+}
